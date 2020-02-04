a4324d08f182110e6aab11270576c0e47d60efe9
Michael Waldo <mbwcogic@yahoo.com>
2020-02-04 17:18:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,13 @@
 html = File.read('fixtures/kickstarter.html')
  require "nokogiri"
 require "pry"
+
+# projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("ul.project-meta span.location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 kickstarter = Nokogiri::HTML(html)
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -17,12 +24,7 @@ def create_project_hash
   # return the projects hash
   projects
 end
-  # projects: kickstarter.css("li.project.grid_4")
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("ul.project-meta span.location-name").text
-  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  
   binding.pry
 end
  
